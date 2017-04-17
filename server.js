52b4ffed0583af677c49aa85511fa9804fb348e3
Mychal Simka <mychalsimka@gmail.com>
2017-04-17 03:06:46 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+
+res.send('Hello world!');
+
+});
+
+app.listen(8080, function() {
+
+console.log('Server listening on port 8080!');
+
+});
